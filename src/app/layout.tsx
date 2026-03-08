3410b10bf5a2ebdfb5d83b65cdee136efe610418
ayandafadar <technoayan55@gmail.com>
2026-03-09 00:19:02 +0530

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Portfolio',
   description: 'Developer portfolio',
+  icons: {
+    icon: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({
